494349a76d4a2b1d5ab6a05f536cbd2b9c13ff63
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2023-06-28 05:20:22 +0100

    updated compiled changes to ts files

---
+++ public/finance.js
@@ -0,0 +1,22 @@
+const me = {
+    name: 'shaun',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+import { Invioce } from "./classes/invoice.js";
+const invOne = new Invioce("mario", "work on the mario website", 250);
+const form = document.querySelector(".new-item-form");
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
