fbf17f734725c09c256a462f63ffaec94e727f2e
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2023-06-28 05:19:43 +0100

    Updated finance.ts

---
+++ src/finance.ts
@@ -0,0 +1,32 @@
+interface IsPerson { 
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name: 'shaun',
+    age: 30,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+import { Invioce } from "./classes/invoice.js";
+
+const invOne = new Invioce("mario", "work on the mario website", 250);
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
